d1d149fc89b2bb6e360340e68a45cc535e8515c3
SupportALM <support@alucremusket.com>
2025-04-15 13:20:50 +0100

    Add built ES module dist/hello-remote.js for GitHub Pages

---
+++ dist/hello-remote.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HelloRemote() {
+  return /*#__PURE__*/React.createElement("div", {
+    style: {
+      padding: 20,
+      fontSize: 24
+    }
+  }, "Hello from my remote tool!");
+}
+
+export { HelloRemote as default };
